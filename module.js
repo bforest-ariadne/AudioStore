274c811b5ac03cd57295d1ac9a28b34227c3f63d
Ben Forest <ben@ben-forest.com>
2018-11-02 20:39:22 -0400

    fix umd dependencies. add module.js

---
+++ module.js
@@ -0,0 +1,10 @@
+const AudioStore = require('./lib/audiostore');
+const StreamCoordinator = require('./lib/streamcoordinator');
+const Player = require('./lib/player');
+
+
+module.exports = {
+  AudioStore,
+  StreamCoordinator,
+  Player
+}
